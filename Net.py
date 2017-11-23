b7e97f448ccd38f505a1aca5bec74e547f3b3d40
Lin_chun <lzy2lcy@gmail.com>
2017-11-23 10:53:35 +0800

    Siamese and Reidentify network

--- Net.py
+++ Net.py
@@ -15,7 +15,7 @@ class SiameseNet(nn.Module):
             nn.ReflectionPad2d(1),#表示对边缘进行pad增加
             nn.Conv2d(1,4,kernel_size=3),#conv层
             nn.ReLU(inplace=True),#表示新创建一个对象并直接对这个对象进行修改？？
-            nn.BatchNorm2d(4),# With Learnable Parameters;num_features: num_features from an expected input of size batch_size x num_features x height x width作用？？
+            nn.BatchNorm2d(4),# With Learnable Parameters;num_features: num_features from an expected input of size batch_size x num_features x height x width
             nn.Dropout2d(p=.2),#p:probability of an element to be zeroed
 
             nn.ReflectionPad2d(1),
@@ -50,48 +50,56 @@ class SiameseNet(nn.Module):
         output2=self.forward_once(input2)
         return output1,output2
 
-
-#original face cnn network
+#original face cnn network 
 class OriNet(nn.Module):
     def __init__(self,bits_len,pretrained=False,**kwargs):
         super(OriNet,self).__init__()
-        self.model=ResNet(BasicBlock,[2,2,2,2],**kwargs)
+        self.bits_len=bits_len
+        self.resnetmodel=ResNet(BasicBlock,[2,2,2,2],**kwargs)
         if pretrained:
             parameters=model_zoo.load_url(model_urls['resnet18'])
             self.model.load_state_dict(parameters)
-        self.model.avgpool=None
-        self.model.fc1=nn.Linear(512*3*4,512)
-        self.model.fc2=nn.Linear(512,512)
-        self.model.classifier=nn.Linear(512,num_classes)
-        self.register_buffer('ranking',torch.zeros(num_classes,512))
-        self.bits_len=bits_len
+        self.resnetmodel.avgpool=None
+        self.resnetmodel.fc1=nn.Linear(512*3*4,512)
+        self.resnetmodel.fc2=nn.Linear(512,512)
+        self.resnetmodel.classifier=nn.Linear(512,bits_len)
+        self.register_buffer('centers',torch.zeros(bits_len,512))
 
-    def forward(self,x):
-        x=self.model.conv1(x)
-        x=self.model.bn1(x)
-        x=self.model.relu(x)
-        x=self.model.maxpool(x)
+    def forward(self,input1):
+        input1=self.resnetmodel.conv1(input1)
+        input1=self.resnetmodel.bn1(input1)
+        input1=self.resnetmodel.relu(input1)
+        input1=self.resnetmodel.maxpool(input1)
 
-        x=self.model.layer1(x)
-        x=self.model.layer2(x)
-        x=self.model.layer3(x)
-        x=self.model.layer4(x)
+        input1=self.resnetmodel.layer1(input1)
+        input1=self.resnetmodel.layer2(input1)
+        input1=self.resnetmodel.layer3(input1)
+        input1=self.resnetmodel.layer4(input1)
 
-        x=x.view(x.size(),-1)
-        x=self.model.fc1(x)
+        input1=input1.view(input1.size(),-1)
+        input1=self.resnetmodel.fc1(input1)
         #feature for center loss
-        x=self.model.fc2(x)
-        self.features=x
-        x=self.model.classifier(x)
-        return F.log_softmax(x)
+        input1=self.resnetmodel.fc2(input1)
+        self.features=input1
+        input1=self.resnetmodel.classifier(input1)
+        output1=F.log_softmax(input1)
 
-#de-identification face model
-#class DeidenNet(nn.Module):
-#    def __init__(self):
-#        super(DeidenNet,self).__init__()
-#        nn.Linear()
+        return output1
 
+# de-identification face model
+class DeidenNet(nn.Module):
+    def __init__(self,bits_len,pretrained=False,**kwargs):
+        super(DeidenNet,self).__init__()
+        self.deidentifymodel.fc1=nn.Linear(8*100*100,500)
+        self.deidentifymodel.fc2=nn.Linear(500,bits_len)
     
-#    def forward(self):
+    def forward(self,input2):         
+        #full connect network
+        input2=self.deidentifymodel.fc1(input2).clamp(min=0)
+        input2=F.relu(input2)
+        input2=self.deidentifymodel.fc2(input2)
+        input2=F.relu(input2)
+        output2=F.log_softmax(input2)
 
+        return output2
 
